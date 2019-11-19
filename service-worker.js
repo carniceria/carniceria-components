0bbf891ceb1532ae926ea5f5760ed6e7e518cee2
hectoruch <hector.arce@gruposantander.com>
2019-11-19 21:42:37 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/my-library/index.html", {
+workbox.routing.registerNavigationRoute("/carniceria/carniceria.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
